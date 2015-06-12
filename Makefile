be3ea54f29a163ba482d2f8e771eb861e7919112
Owen Rodley <Strigoides@gmail.com>
2015-06-12 22:48:24 +1200

    Pick a starting word randomly (using reservoir sampling).

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
 NAME := ttc
 CC ?= clang
 CFLAGS := -std=c99 -Werror -Wall -Wextra -Wstrict-prototypes
+
 CFLAGS += $(shell pkg-config --cflags sdl2)
 LDFLAGS := $(shell pkg-config --libs sdl2)
 OBJS := $(patsubst src/%.c, src/%.o, $(wildcard src/*.c))
 
 .PHONY: all
 all: $(NAME)
+
+.PHONY: debug
+debug: CFLAGS += -ggdb3
+debug: $(NAME)
 
 $(NAME): $(OBJS)
 	$(CC) $(LDFLAGS) $^ -o $@
--- src/main.c
+++ src/main.c
@@ -1,40 +1,67 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 #include <SDL2/SDL.h>
 #include "anagram.h"
 #include "words.h"
+
+void most_and_fewest(WordTree *tree)
+{
+	size_t max_count = 0;
+	char *max_word;
+	size_t min_count = (size_t)-1;
+	char *min_word;
+	for (size_t i = 0; words[i] != NULL; i++) {
+		if (strlen(words[i]) != 6)
+			continue;
+
+		AnagramsResult anagrams = find_all_anagrams(tree, words[i]);
+		if (anagrams.count > max_count) {
+			max_count = anagrams.count;
+			max_word = words[i];
+		} else if (anagrams.count < min_count) {
+			min_count = anagrams.count;
+			min_word = words[i];
+		}
+	}
+
+	printf("Fewest anagrams: \"%s\": %zd\n", min_word, min_count);
+	printf("Most anagrams: \"%s\": %zd\n", max_word, max_count);
+}
+
+char *random_word(char **word_list, unsigned int length)
+{
+	char *selected_word;
+	int valid_words = 0;
+	for (int i = 1; word_list[i] != NULL; i++) {
+		char *word = word_list[i];
+		if (strlen(word) == length) {
+			valid_words++;
+			float probability = (float)rand() / RAND_MAX;
+			if (valid_words == 1 || probability < (1.0 / valid_words))
+				selected_word = word;
+		}
+	}
+
+	return selected_word;
+}
 
 int main(int argc, char *argv[])
 {
 	WordTree *tree = word_list_to_tree(words);
 
-	if (argc == 1) {
-		size_t max_count = 0;
-		char *max_word;
-		size_t min_count = (size_t)-1;
-		char *min_word;
-		for (size_t i = 0; words[i] != NULL; i++) {
-			if (strlen(words[i]) != 6)
-				continue;
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	srand(t.tv_usec * t.tv_sec);
 
-			AnagramsResult anagrams = find_all_anagrams(tree, words[i]);
-			if (anagrams.count > max_count) {
-				max_count = anagrams.count;
-				max_word = words[i];
-			} else if (anagrams.count < min_count) {
-				min_count = anagrams.count;
-				min_word = words[i];
-			}
-		}
-
-		printf("Fewest anagrams: \"%s\": %zd\n", min_word, min_count);
-		printf("Most anagrams: \"%s\": %zd\n", max_word, max_count);
-
-		return 0;
-	} else if (argc != 2) {
+	char *word;
+	if (argc == 1)
+		word = random_word(words, 6);
+	else if (argc == 2)
+		word = argv[1];
+	else
 		return 1;
-	}
-	char *word = argv[1];
+
 	if (strlen(word) > 6)
 		return 2;
 
